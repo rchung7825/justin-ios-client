04088b493d74bf95a00be545a01726da8beb35ef
Ray Chung <rchung725@gmail.com>
2024-04-05 15:27:18 -0400

    ios client started, after the meeting with Justin's teachers.

---
+++ justin-ios-client/justin-ios-client/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  justin-ios-client
+//
+//  Created by Raymond Yongsub Chung on 4/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+
+#Preview {
+    ContentView()
+}
+
+
---
+++ justin-ios-client/justin-ios-client/justin_ios_clientApp.swift
@@ -0,0 +1,17 @@
+//
+//  justin_ios_clientApp.swift
+//  justin-ios-client
+//
+//  Created by Raymond Yongsub Chung on 4/5/24.
+//
+
+import SwiftUI
+
+@main
+struct justin_ios_clientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
