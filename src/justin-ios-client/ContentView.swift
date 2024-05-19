174a3d94fc4e71078cc106af55e8dee53b0d3527
Ray Chung <rchung725@gmail.com>
2024-05-19 16:34:57 -0400

    stop playing music, before playing to the next.

--- src/justin-ios-client/ContentView.swift
+++ src/justin-ios-client/ContentView.swift
@@ -80,6 +80,7 @@ struct ContentView: View {
         }
         do {
             audioPlayer = try AVAudioPlayer(contentsOf: mp3file)
+            audioPlayer!.stop()
             audioPlayer!.play()
             
             ringBuffer.append(word)
