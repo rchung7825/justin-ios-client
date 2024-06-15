4baa570804c9a8f5aa4263c7f4b86bc9c27a6b8b
Ray Chung <rchung725@gmail.com>
2024-06-15 10:50:10 -0400

    2nd try, begin.  Start over.

---
+++ justin-ios-client2/justin-ios-client2/justin_ios_client2App.swift
@@ -0,0 +1,17 @@
+//
+//  justin_ios_client2App.swift
+//  justin-ios-client2
+//
+//  Created by Raymond Chung on 6/15/24.
+//
+
+import SwiftUI
+
+@main
+struct justin_ios_client2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
